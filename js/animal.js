ba4d30734f049b57092ddf912bcaf811430a2fc0
westyju8 <77226740+westyju8@users.noreply.github.com>
2021-04-20 13:15:52 +0900

    0420 0115

---
+++ js/animal.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+
+  var num = 1
+  function change(x){
+    if(x){
+      if(num == 15) return;
+      x++;
+    } else {
+      if(num == 1) return;
+      x--;
+    }
+
+  }
+
+
+
+
+});//end
